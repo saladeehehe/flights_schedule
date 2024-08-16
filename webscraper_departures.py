346c24199aa4f3cb896d272e0ed3f138121337f7
saladeehehe <106911395+saladeehehe@users.noreply.github.com>
2024-08-16 12:31:58 +0800

    Update webscraper_departures.py

--- webscraper_departures.py
+++ webscraper_departures.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, timedelta
 import json
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Set up Chrome options for headless mode
@@ -21,6 +21,7 @@ wait = WebDriverWait(driver, 15)
 # Open the flight details webpage
 driver.get("https://www.changiairport.com/en/flights/departures.html")
 
+# Load existing data
 try:
     with open("departures_data.json", "r") as f:
         existing_data = json.load(f)
@@ -28,7 +29,7 @@ try:
 except FileNotFoundError:
     existing_data = []
 
-# Load seen_data.txt
+# Load seen flights
 try:
     with open("seen_data.txt", "r") as f:
         seen_flights = set(line.strip() for line in f.readlines())
@@ -46,53 +47,38 @@ def choose_date(date):
     try:
         # Click the calendar input to open the date picker
         calendar_input = driver.find_element(By.CSS_SELECTOR, 'div.react-datepicker__input-container input[type="button"]')
-        if calendar_input:
-            print("Calendar button found")
         scroll_and_click(calendar_input)
 
         # Wait for the date picker to be visible
         date_picker = WebDriverWait(driver, 20).until(
             EC.visibility_of_element_located((By.CLASS_NAME, 'react-datepicker__month'))
         )
-        print("Date picker is visible")
 
         # Generate the correct class name with two digits for the day
-        day_str = f"{date.day:03d}"
+        day_str = f"{date.day:02d}"
         target_day_class = f"react-datepicker__day--{day_str}"
-        
-        # Debug: Print all available day elements
-        day_elements = driver.find_elements(By.CLASS_NAME, 'react-datepicker__day')
-        for elem in day_elements:
-            print(f"Found day element: {elem.text} with class: {elem.get_attribute('class')}")
-        
+
         # Locate and click the target day element
         date_element = driver.find_element(By.CLASS_NAME, target_day_class)
         scroll_and_click(date_element)
-        print("Date clicked")
-        #driver.save_screenshot("screenshot1.png")  # Save a screenshot for debugging
-        
+
         # Wait for the flights list to update
         WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'div.data.flightlist'))  # Adjust the selector based on actual flights list container
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div.data.flightlist'))
         )
-        print("Flights list should be updated now")
-        #driver.save_screenshot("screenshot2.png")  # Save a screenshot to confirm the flights list
 
     except Exception as e:
         print(f"Date picker not visible or error occurred: {e}")
-        driver.save_screenshot("Exception in date picker.png")  # Save a screenshot for debugging
-
+        driver.save_screenshot("Exception_in_date_picker.png")  # Save a screenshot for debugging
 
 def create_flight_id(flight_number, departure_time):
-        return f"{flight_number}_{departure_time}"
+    return f"{flight_number}_{departure_time}"
 
-
-###################### PROCESS FLIGHTS WITHIN DAY 
 def process_flights(date, flight_elements, start_index, last_time, stop_loop, invalid_gate_count):
     global seen_flights, departure_flights_data
     last_processed_index = start_index
     time_threshold = timedelta(hours=2)
-    
+
     for idx, flight_element in enumerate(flight_elements[start_index:], start=start_index):
         try:
             # Extracting the time
@@ -128,23 +114,25 @@ def process_flights(date, flight_elements, start_index, last_time, stop_loop, in
             flight_number = flight_element.find_element(By.CSS_SELECTOR, 'span.airport__flight-number').text.strip()
             airline_name = flight_element.find_element(By.CSS_SELECTOR, 'span.airport__name').text.strip()
             destination = flight_element.find_element(By.CSS_SELECTOR, 'div.airport-name').text.strip()
-            terminal = flight_element.find_element(By.CSS_SELECTOR, 'div.flightlist__item-terminal').text.strip() or 'Unknown'        
+            terminal = flight_element.find_element(By.CSS_SELECTOR, 'div.flightlist__item-terminal').text.strip() or 'Unknown'
+            
             try:
                 # Find the gate number container
                 gate_div = flight_element.find_element(By.CSS_SELECTOR, 'div.flightlist__item-boarding div:nth-child(2) span.gate')
                 gate_number = gate_div.text.strip()
-            except Exception as e:
-                gate_number = 'Unknown'  # Assign 'Unknown' if the gate number is not found
+            except Exception:
+                gate_number = 'Unknown'
+                
             if gate_number == 'Unknown':
                 invalid_gate_count += 1
             else:
                 invalid_gate_count = 0
-            
+
             if invalid_gate_count >= 3:
                 print("Terminating scraping due to 3 consecutive flights without a valid gate number.")
                 stop_loop = True
                 break
-    
+
             flight_id = create_flight_id(flight_number, departure_datetime_str)
 
             if flight_id not in seen_flights and gate_number != "Unknown":
@@ -170,21 +158,14 @@ def process_flights(date, flight_elements, start_index, last_time, stop_loop, in
         except Exception as e:
             print("Error processing flight details:", e)
 
-    # check for error in variables
-    print (last_time, stop_loop, last_processed_index, invalid_gate_count)
     return last_time, stop_loop, last_processed_index, invalid_gate_count
 
-
-#############################################################
 def scrape_flights_for_date(date):
-
-    # Initialize variables
     last_time = None
-    stop_loop = False # to ensure that next day flights are not added in, prevent duplication
+    stop_loop = False
     last_processed_index = 0
-    invalid_gate_count = 0 
+    invalid_gate_count = 0
 
-    #select date for flight schedule
     choose_date(date)
 
     while not stop_loop:
@@ -217,8 +198,8 @@ def scrape_flights_for_date(date):
 
 # Main execution
 departure_flights_data = []
-seen_flights = set()
 start_date = datetime.today()
+
 # Loop through the next 3 days
 for day in range(3):
     target_date = start_date + timedelta(days=day)
@@ -231,7 +212,6 @@ print(f"Total flights collected: {len(departure_flights_data)}")
 driver.quit()
 
 existing_data.extend(departure_flights_data)
-
 
 # Save updated departure_flights_data to departures_data.json
 with open("departures_data.json", "w") as json_file:
