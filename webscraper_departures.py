17ee12c2935ff3d11904866c96e54b3888d768ca
saladeehehe <106911395+saladeehehe@users.noreply.github.com>
2024-08-16 12:52:49 +0800

    Update webscraper_departures.py

--- webscraper_departures.py
+++ webscraper_departures.py
@@ -1,5 +1,4 @@
 import json
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -7,7 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, timedelta
-from selenium.webdriver.common.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Configure Chrome options
@@ -17,11 +15,9 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
 # Initialize WebDriver with options
-service = ChromeService(executable_path=ChromeDriverManager().install())
+service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 wait = WebDriverWait(driver, 15)
-
-
 
 # Open the flight details webpage
 driver.get("https://www.changiairport.com/en/flights/departures.html")
